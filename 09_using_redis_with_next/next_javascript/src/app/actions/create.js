b6d70516e912562abd8418bcdb2a042ebdf9ece0
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-03-07 20:30:30 +0100

    09 using redis with next: Javascript solution

---
+++ 09_using_redis_with_next/next_javascript/src/app/actions/create.js
@@ -0,0 +1,28 @@
+"use server";
+
+/* 
+
+- store a book as a hash in redis db
+- create a book id
+- can be books:1, books:2, etc
+- save new hash for the book
+
+*/
+
+import { client } from "@/lib/database";
+import { redirect } from "next/navigation";
+
+export async function createBook(formData) {
+  const { title, rating, author, blurb } = Object.fromEntries(formData);
+
+  const bookId = new Date().getTime().toString();
+
+  await client.hSet(`books:${bookId}`, {
+    title,
+    rating,
+    author,
+    blurb,
+  });
+
+  redirect("/");
+}
